ef2755ddcbabfaeeea77aa0b3f7e58afcf2c04c4
Sally Maki <sallymaki@gmail.com>
2017-03-07 13:51:17 -0800

    Add drawing of a Tic Tac Toe Board

---
+++ public/js/ticTacToe.js
@@ -0,0 +1,58 @@
+var ticTacToe = {
+  winingCombos: [
+    [1, 2, 3], // Horizontals
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7], // Verticals
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9], // Diagonals
+    [7, 5, 3]
+  ],
+  playerOneScore: 0,
+  playerTwoScore: 0,
+  clearBoard: function() {
+    this.currentBoard = {
+      1: '',
+      2: '',
+      3: '',
+      4: '',
+      5: '',
+      6: '',
+      7: '',
+      8: '',
+      9: ''
+    };
+  },
+
+};
+
+function drawBoard() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  //ctx.fillStyle = "#FF0000";
+  ctx.strokeStyle = "#fff";
+  ctx.lineWidth = 1;
+  // Vertical Lines
+  ctx.beginPath();
+  ctx.moveTo(133, 0);
+  ctx.lineTo(133, 400);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(266, 0);
+  ctx.lineTo(266, 400);
+  ctx.closePath();
+  ctx.stroke();
+  // Horizontal Lines
+  ctx.beginPath();
+  ctx.moveTo(0, 133);
+  ctx.lineTo(400, 133);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(0, 266);
+  ctx.lineTo(400, 266);
+  ctx.closePath();
+  ctx.stroke();
+};
